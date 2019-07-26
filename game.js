02193ae07b01817cd9f9812a196f4c47c4ca2c02
Nam Dao <nhatnamdao@gmail.com>
2019-07-26 09:53:17 +0700

    added swipe detect for changing menu options. switched mobile flapping to trigger on touchend in order to accommodate swipe detection

--- game.js
+++ game.js
@@ -15,7 +15,9 @@ function Game(context, width, height) {
    this.width = width;
    this.height = height;
    this._delta = 1000/100;
-
+   
+   this.swiped = false;
+   this.swipedir = "";
    this.powerup_timer = 0;
    // Background image -- 1157 x 288. 
    this.background = new Image();
@@ -35,6 +37,8 @@ function Game(context, width, height) {
    this.over_menu.init();
    this.newhighscore_menu = new NewHighscoreMenu(width, height);
    this.newhighscore_menu.init();
+   this.instructions_menu = new InstructionsMenu(width, height, this);
+   this.instructions_menu.init();
 
    // sound manager
    this.soundManager = new SoundManager();
@@ -47,7 +51,8 @@ function Game(context, width, height) {
    this.player_count = 1;
    this.player_points = 0;
    this.player_name = "";
-   this.point_multiplier = 1;  
+   this.point_multiplier = 1;
+   this.save_dy = 0;  
 
    // bird object
    this.bird = new Bird();
@@ -56,7 +61,7 @@ function Game(context, width, height) {
    this.pipes = [];
 
    //power cube object
-   this.powercube = new PowerCube(300, 250);
+   this.powercube = new PowerCube(1000, 400);
 
    /**
     * Reset game state variables
@@ -75,8 +80,8 @@ function Game(context, width, height) {
 
       var y = Math.random()*(HEIGHT - this.powercube.length*2) + this.powercube.length;
       var x = 1315 + PIPE_SPACING*(5+(Math.random() * 1)) ;
-      this.powercube.x = 300
-      this.powercube.y = 250;
+      this.powercube.x = 1000;
+      this.powercube.y = 400;
    }
 
    /**
@@ -117,12 +122,12 @@ function Game(context, width, height) {
          //once bird is off the screen...then switch game states...
       }else if(this.game_state == "high_score"){
          this.bird.update(delta);
-      }else if(this.game_state == "powerphase"){
+   /*   }else if(this.game_state == "powerphase"){
 
          // momentarily stop updating objects         
 
-
-      }else if(this.game_state == "playing" ){
+*/
+      }else if(this.game_state == "playing"){
          /* update background */
          if(this.powerup_timer > 0){
             this.powerup_timer = this.powerup_timer - delta;
@@ -179,10 +184,18 @@ function Game(context, width, height) {
      
             if(this.bird.collidesPower( this.powercube ) ){
                   this.powercube.random_power = Math.floor(Math.random() * this.powercube.number_of_powerups);
-                  this.game_state = "powerphase";
+                  //this.game_state = "powerphase";
                   this.soundManager.playSound("powerup");
+                  this.save_dy = this.bird.dy;
+
                   this.powercube.fillStyle = "rgba(200,0,0,.4)";
                   var game = this;
+
+                  if(game.powercube.random_power == 0){
+                     game.powerDoublePoints();
+                  } else if(game.powercube.random_power == 1){
+                     game.powerEasyControl();
+                  }
 
                   setTimeout(function(){
                      game.powercube.fillStyle = "rgba(0,0,0,0)";
@@ -196,20 +209,14 @@ function Game(context, width, height) {
                   setTimeout(function(){
                      game.powercube.fillStyle = "rgba(200,0,0,.4)";
                   },1000);
-
+                  
                   setTimeout(
                      function(){ 
                         game.powercube.fillStyle = "rgba(0,0,0,0)";
                         game.powercube.x = game.pipes[game.pipes.length-1].x + 140;
                         game.powercube.y = 100 + Math.random()*(HEIGHT-200);  
-                           if(game.powercube.random_power == 0){
-                              game.powerDoublePoints();
-                           } else if(game.powercube.random_power == 1){
-                              game.powerEasyControl();
-                           }
-                        game.game_state = "playing";
                      },
-                  1250);
+                  1000);
          }
          
 
@@ -239,8 +246,8 @@ function Game(context, width, height) {
             this.menu.render(this.ctx);
          }else if(this.game_state == "highscore"){
             this.highscore_menu.render(this.ctx);
-         }else if(this.game_state == "gameinstructions"){
-            this.gameinstructions_menu.render(this.ctx);
+         }else if(this.game_state == "instructions"){
+            this.instructions_menu.render(this.ctx);
          }else{
             /* 
                Game scene
@@ -252,7 +259,8 @@ function Game(context, width, height) {
 
             this.drawScore();
             if(this.point_multiplier == 2){
-               ctx.fillText(""+Math.floor(this.powerup_timer/1000) + "."+Math.floor(this.powerup_timer/10)%100, 280, 50);
+               this.drawX2();
+               ctx.fillText(""+Math.floor(this.powerup_timer/1000) + "."+Math.floor(this.powerup_timer/10)%100, 150, 100);
                ctx.fillStyle = "white";
             }
       
@@ -264,12 +272,6 @@ function Game(context, width, height) {
                this.over_menu.render(this.ctx);
             }else if(this.game_state == "new_highscore"){
                this.newhighscore_menu.render(this.ctx);
-            }else if(this.game_state == "powerphase"){
-               if(this.powercube.random_power == 0){
-                  this.drawX2();
-               } else if(this.powercube.random_power == 1){
-                  this.drawEzControl();
-               }
             }
       }
 
@@ -277,20 +279,19 @@ function Game(context, width, height) {
 
    this.drawX2 = function(){
       ctx.font = "900 16px 'Press Start 2P'";
-      ctx.fillText("Double Points", 40, 100);
-      ctx.fillStyle="black";
+      ctx.fillText("Double Points", 100, 120);
    }
 
    this.drawEzControl = function(){
       ctx.font = "900 16px 'Press Start 2P'";
-      ctx.fillText("Easy Control", 40, 100);
-      ctx.fillStyle="black";
+      ctx.fillText("Easy Control", 100, 100);
    }
    this.drawInstructions = function(){
+      ctx.save();
       ctx.font = "900 16px 'Press Start 2P'";
       ctx.fillStyle="black";
       ctx.fillText("Press SPACE to continue", 20, 400);
-      ctx.filStyle = "white";
+      ctx.restore();
    }
 
 
@@ -339,26 +340,51 @@ function Game(context, width, height) {
    }
 
 
-window.ontouchstart = function(e){
+window.ontouchend = function(e){
 
-   if(game.game_state == "playing"){
-         game.bird.flap();
-         game.soundManager.playSound("jump");
-   }else if(game.game_state == "menu"){
-      if(game.menu.current_option == 0){
-         game.startGame();
-      }
-   }else if(game.game_state =="game_over"){
-         
-         if(game.over_menu.current_option == 0){
+   if(!game.swiped){
+      if(game.game_state == "playing"){
+            game.bird.flap();
+            game.soundManager.playSound("jump");
+      }else if(game.game_state == "menu"){
+         if(game.menu.current_option == 0){
+            game.startGame();
+         }else if(game.menu.current_option == 1){
+            game.game_state = "highscore";
+         }else if(game.menu.current_option == 2){
+            game.game_state = "instructions";
+         }
+      }else if(game.game_state =="game_over"){
+            
+            if(game.over_menu.current_option == 0){
+               game.game_state = "menu";
+            } 
+      }else if(game.game_state == "instructions"){
+         game.game_state = "menu"
+      }else if(game.game_state =="highscore"){ 
+         if(game.highscore_menu.current_option == 0){
             game.game_state = "menu";
-         } 
+         }else if(game.highscore_menu.current_option == 1){
+            game.highScoreManager.clearScores();
+            game.highscore_menu.reload();
+         }
+      }
+   }else{
+      if(game.game_state == "menu"){
+         if(game.swipedir == "up"){
+            game.menu.optionUp();
+
+         }else if (game.swipedir == "down"){
+            game.menu.optionDown();
+         }
+      }
+      //do swipe
+      game.swiped = false;
    }
 
 }
 
 window.onkeydown = function(e){
-
    if(game.game_state == "new_highscore"){
       //e.preventDefault();
       var alphabet = "-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -433,8 +459,10 @@ window.onkeydown = function(e){
          }else if(game.menu.current_option == 1){
             game.game_state = "highscore";
          }else if(game.menu.current_option == 2){
-            game.game_state = "gameinstructions";
+            game.game_state = "instructions";
          }
+      }else if(game.game_state == "instructions"){
+         game.game_state = "menu"
       }else if(game.game_state =="highscore"){ 
          if(game.highscore_menu.current_option == 0){
             game.game_state = "menu";
@@ -443,8 +471,6 @@ window.onkeydown = function(e){
             game.highscore_menu.reload();
 
          }
-
-
 
       }else if(game.game_state =="game_over"){
          
@@ -469,3 +495,22 @@ window.onkeydown = function(e){
 // create game and start game
 var game = new Game(ctx, WIDTH, HEIGHT);
 game.loop();
+
+window.mobilecheck = function() {
+  var check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;
+};
+
+if(mobilecheck()){
+   swipedetect(canvas, function(swipedir){
+      if(swipedir != "none"){
+         game.swiped = true;
+         game.swipedir = swipedir;
+      }else{
+         game.swiped = false;
+      }
+   });     
+}
+
+
